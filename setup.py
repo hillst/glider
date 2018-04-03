1dd9624cae0f506a81a9747f160a908b8a9e77b7
Steven Hill <hill2steve@gmail.com>
2018-04-03 15:26:59 -0400

    added setup
    Signed-off-by: Steven Hill <hill2steve@gmail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='glider',
+    version='0.1.0',
+    packages=['glider', 'glider.viz', 'glider.data', 'glider.utils'],
+    url='github.com/hillst/glider',
+    license='MIT',
+    author='Steven Kothen-Hill',
+    author_email='hill2steve@gmail.com',
+    description='Toolkit for analyzing cfDNA trinucleotides.'
+)
